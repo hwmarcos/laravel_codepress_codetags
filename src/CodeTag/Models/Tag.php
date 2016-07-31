9e5d20a5babb89d59fb94bbf5bac1db1dd8b689c
helder <helderwmarcos@gmail.com>
2016-07-31 16:27:00 -0300

    Second Commit

--- src/CodeTag/Models/Tag.php
+++ src/CodeTag/Models/Tag.php
@@ -4,8 +4,8 @@ namespace CodePress\CodeTag\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Tag extends Model {
-
+class Tag extends Model
+{
     protected $table = 'codepress_tags';
     private $validator;
 
@@ -16,20 +16,28 @@ class Tag extends Model {
 
     public function getValidator()
     {
-        return $this->validator;
+        return $this->validator;    
     }
 
-    public function taggable() {
+    public function taggable()
+    {
         return $this->morphTo();
+    }
+
+    public function posts()
+    {
+        return $this->morphedByMany(Post::class, 'taggable', 'codepress_taggables');
     }
 
     protected $fillable = ['name', 'parent_id'];
 
-    public function parent(){
+    public function parent()
+    {
         return $this->belongsTo(Tag::class);
     }
 
-    public function children() {
+    public function children()
+    {
         return $this->hasMany(Tag::class, 'parent_id');
     }
 
--- src/CodeTag/Providers/CodeTagServiceProvider.php
+++ src/CodeTag/Providers/CodeTagServiceProvider.php
@@ -4,16 +4,19 @@ namespace CodePress\CodeTag\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
-class CodeTagServiceProvider extends ServiceProvider {
+class CodeTagServiceProvider extends ServiceProvider
+{
 
-    public function boot() {
+    public function boot()
+    {
         $this->publishes([__DIR__ . '/../../resources/migrations/' => base_path('database/migrations')], 'migrations');
         $this->loadViewsFrom(__DIR__ . '/../../resources/views/codetag/', 'codetag');
         require(__DIR__ . '/../../routes.php');
     }
 
-    public function register() {
-        
+    public function register()
+    {
+
     }
 
 }
